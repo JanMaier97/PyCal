f1156e61b5fa3e402557d97cfa97526e0e27b65c
Jan Maier <janmaier1997@outlook.de>
2018-05-17 13:22:39 +0200

    Implemented tokenize.

--- tokenize.py
+++ tokenize.py
@@ -1,3 +1,13 @@
+import re
+
 
 def tokenize(string):
-    pass
+    """Return a list of tokens of a mathimatical expression."""
+    pattern = r"(\b\w*[\.]?\w+\b|[()+*%\/\-])"
+    return re.findall(pattern, string)
+
+
+# simple testing
+if __name__ == "__main__":
+    expression = "10 * (20 + 100) / sin(x + 8)"
+    print(tokenize(expression))
