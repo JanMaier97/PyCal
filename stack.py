c96e225f4bbcec590cf58a89e384956343409bf7
Jan Maier <janmaier1997@outlook.de>
2018-05-17 11:15:53 +0200

    Updated the string representation of the class.

--- stack.py
+++ stack.py
@@ -1,3 +1,5 @@
+import copy
+
 
 class Stack:
     ERROR_EMPTY_STACK = "Error, stack is empty"
@@ -24,19 +26,19 @@ class Stack:
         """Return True if the stack is empty, else return False."""
         return self.elements == []
 
-    def __str__(self):
+    def __repr__(self):
         """Return the string representation of the stack."""
-        value_str = self._stack_to_string(self)
+        value_str = self._stack_to_string(copy.deepcopy(self))
         line = "-" * len(value_str)
 
-        return line + "\n" + value_str + "\n" + line
+        return "\n".join([line, value_str, line])
 
     def _stack_to_string(self, stack):
         """Return a string of the stack values seperated by "|"."""
-        value_str = "|"
-        while not self.is_empty():
-            value_str += " " + str(self.top()) + " |"
-            self.pop()
+        value_str = " |"
+        while not stack.is_empty():
+            value_str = "| " + str(stack.top()) + value_str
+            stack.pop()
 
         return value_str
 
@@ -50,9 +52,8 @@ if __name__ == "__main__":
     test_stack = Stack()
     for i in range(MAX_COUNT):
         test_stack.push(i + 1)
-        str(test_stack)
+        print(test_stack)
 
     while not test_stack.is_empty():
-        print("Removing: " + str(test_stack.top()))
         test_stack.pop()
-        str(test_stack)
+        print(test_stack)
